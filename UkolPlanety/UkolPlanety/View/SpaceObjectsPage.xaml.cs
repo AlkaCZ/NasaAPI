e05f58576e91738224689122887f865f6309f71f
Petr <petrjelinek35@seznam.cz>
2021-01-14 15:49:36 +0100

    Working view button

--- UkolPlanety/UkolPlanety/View/SpaceObjectsPage.xaml.cs
+++ UkolPlanety/UkolPlanety/View/SpaceObjectsPage.xaml.cs
@@ -18,6 +18,7 @@ namespace UkolPlanety.View
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class SpaceObjectsPage : ContentPage
     {
+        ItemTappedEventArgs item;
         public SpaceObjectsPage()
         {
 
@@ -31,23 +32,39 @@ namespace UkolPlanety.View
         {
             if (e.Item == null)
                 return;
-            Page p = new ASpaceObjectPage(((((ListView)sender).SelectedItem) as Models.ASpaceObject), (BindingContext as SpaceObjects).AllSpaceObjects);
-            NavigationPage np = new NavigationPage(p);
-            await Application.Current.MainPage.Navigation.PushAsync(np);
+                item = e;
+                Page p = new ASpaceObjectPage(((((ListView)sender).SelectedItem) as Models.ASpaceObject), (BindingContext as SpaceObjects).AllSpaceObjects);
+                NavigationPage np = new NavigationPage(p);
+                await Application.Current.MainPage.Navigation.PushAsync(np);
 
-            if ((sender as ListView) != null)
-                (sender as ListView).SelectedItem = null;
-
+                if ((sender as ListView) != null)
+                    (sender as ListView).SelectedItem = null;
+            
         }
-
+        
         async void InfoButt_Clicked(object sender, EventArgs e)
         {
 
-                //Page p = new ASpaceObjectPage() as Models.ASpaceObject), (BindingContext as SpaceObjects).AllSpaceObjects);
-                //NavigationPage np = new NavigationPage(p);
-                //await Application.Current.MainPage.Navigation.PushAsync(np);
-            
+            Button button = (Button)sender;
+            StackLayout listViewItem = (StackLayout)button.Parent;
+            ASpaceObject spaceObject = new ASpaceObject();
+            Label labelName = (Label)listViewItem.Children[0];
+            Label labelSize = (Label)listViewItem.Children[1];
+            Label labelSpeed = (Label)listViewItem.Children[2];
+            Label labelHaiardious = (Label)listViewItem.Children[3];
+            spaceObject.Name = labelName.Text;
+            spaceObject.Size = Convert.ToDouble(labelSize.Text);
+            spaceObject.Speed = Convert.ToDouble(labelSpeed.Text);
+            spaceObject.IsHazerdious = Convert.ToBoolean(labelHaiardious.Text);
+
+            Page p = new ASpaceObjectPage(spaceObject, (BindingContext as SpaceObjects).AllSpaceObjects);
+            NavigationPage np = new NavigationPage(p);
+            await Application.Current.MainPage.Navigation.PushAsync(np);
+
+
 
         }
+
+
     }
 }
